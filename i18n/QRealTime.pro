a33c66170265582ec5a8671183f87dc94f3fc939
Mr.Shiva Reddy <shiva@pdcnew.iirs.gov.in>
2019-12-03 16:49:56 +0530

    Language Update
    Updated ts file for spanish.
    This file needs to be edited by using Qt Linguist.

---
+++ i18n/QRealTime.pro
@@ -0,0 +1,3 @@
+FORMS=..\QRealTime_dialog_base.ui ..\QRealTime_dialog_services.ui ..\QRealTime_dialog_import.ui
+SOURCES= ..\QRealTime.py
+TRANSLATIONS = QRealTime_sp.ts
---
+++ i18n/QRealTime_sp.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>QRealTimeDialogBase</name>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="14"/>
+        <source>QGIS_Real_Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="49"/>
+        <source>URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="61"/>
+        <source>User Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="73"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="85"/>
+        <source>Value</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ServicesDialog</name>
+    <message>
+        <location filename="../QRealTime_dialog_import.ui" line="14"/>
+        <source>QRealTime Import</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_services.ui" line="14"/>
+        <source>QRealTime Setting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_services.ui" line="51"/>
+        <source>Tab 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
