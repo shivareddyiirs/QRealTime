2150b3132298087e624b498e719b964a8f7413e4
Rentsendorj <32505759+Rentsendorj@users.noreply.github.com>
2020-01-06 16:34:04 +0530

    Mongolian Translation

---
+++ i18n/QRealTime_mn.ts
@@ -0,0 +1,204 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="mn_MN">
+<context>
+    <name>Aggregate</name>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="178"/>
+        <source>user</source>
+        <translation>хэрэглэгч</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="178"/>
+        <source>password</source>
+        <translation>нууц үг</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="171"/>
+        <source>lastID</source>
+        <translation>Сүүлчийн ID</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="172"/>
+        <source>sync time</source>
+        <translation>дахин холбогдох хугацаа</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="545"/>
+        <source>Enter url in settings</source>
+        <translation>URL-ийг тохируул</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="555"/>
+        <source>Not able to connect to server</source>
+        <translation>Сервер лүү холбогдсонгүй</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="253"/>
+        <source>Not able to parse form list</source>
+        <translation>Маягтын жагсаалтыг шинжлэх боломжгүй байна</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="274"/>
+        <source>Not able to collect data from server</source>
+        <translation>Серверээс цугласан өгөгдөл татаж чадаагүй</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="384"/>
+        <source>Survey form can&apos;t be created, check layer name</source>
+        <translation>Судалгааны маягтыг үүсгэж чадаагүй,давхаргын нэрийг шалгана уу</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="410"/>
+        <source>Layer is online(</source>
+        <translation>Сүлжээнд давхаргыг оруул</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="413"/>
+        <source>Form exists and can not be updated</source>
+        <translation>Маягт нээгдсэн ба шинэчлэж чадсангүй</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="415"/>
+        <source>Form is not sent</source>
+        <translation>Маягтыг илгээгдээгүй</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="431"/>
+        <source>Not able to collect data from Aggregate</source>
+        <translation>Aggregate-ээс цугласан өгөгдлийг татаагүй</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="506"/>
+        <source>Can&apos;t find &apos;%s&apos; field</source>
+        <translation>&apos;%s&apos; талбар олдсонгүй</translation>
+    </message>
+</context>
+<context>
+    <name>Kobo</name>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="779"/>
+        <source>user</source>
+        <translation>хэрэглэгч</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="780"/>
+        <source>password</source>
+        <translation>нууц үг</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="830"/>
+        <source>last Submission</source>
+        <translation>Сүүлчийн ID</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="635"/>
+        <source>sync time</source>
+        <translation>дахин холбогдох хугацаа</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="681"/>
+        <source>Layer is online(</source>
+        <translation>Сүлжээнд давхарга байна</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="684"/>
+        <source>Form exists and can not be updated</source>
+        <translation>Маягт нээгдсэн ба шинэчлэж чадсангүй</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="720"/>
+        <source>Enter url in settings</source>
+        <translation>URL-ийг тохируул</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="710"/>
+        <source>Invalid url username or password</source>
+        <translation>Вэб хаягт хэрэглэгчийн нэр нууц үг алга</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="726"/>
+        <source>Invalid url,username or password</source>
+        <translation>Вэб хаяг хэрэглэгчийн нэр нууц үг алга</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog.py" line="735"/>
+        <source>not able to connect to server</source>
+        <translation>Сервер лүү холбогдсонгүй</translation>
+    </message>
+</context>
+<context>
+    <name>QRealTime</name>
+    <message>
+        <location filename="../QRealTime.py" line="210"/>
+        <source>QRealTime Setting</source>
+        <translation>QRealTime тохиргоо</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime.py" line="212"/>
+        <source>sync</source>
+        <translation>вэб холбоос</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime.py" line="217"/>
+        <source>import</source>
+        <translation>импорт</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime.py" line="219"/>
+        <source>Make Online</source>
+        <translation>Онлайн болгох</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime.py" line="290"/>
+        <source>sync time</source>
+        <translation>дахин холбогдох хугацаа</translation>
+    </message>
+</context>
+<context>
+    <name>QRealTimeDialogBase</name>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="14"/>
+        <source>QGIS_Real_Time</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="49"/>
+        <source>URL</source>
+        <translation>URL вэб холбоос</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="61"/>
+        <source>User Name</source>
+        <translation>Хэрэглэгч</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="73"/>
+        <source>Password</source>
+        <translation>нууц үг</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_base.ui" line="85"/>
+        <source>Value</source>
+        <translation>Утга</translation>
+    </message>
+</context>
+<context>
+    <name>ServicesDialog</name>
+    <message>
+        <location filename="../QRealTime_dialog_services.ui" line="14"/>
+        <source>QRealTime Setting</source>
+        <translation>QRealTime тохиргоо</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_services.ui" line="51"/>
+        <source>Tab 1</source>
+        <translation>Харилцах цонх 1</translation>
+    </message>
+    <message>
+        <location filename="../QRealTime_dialog_import.ui" line="14"/>
+        <source>QRealTime Import</source>
+        <translation>QRealTime импорт</translation>
+    </message>
+</context>
+</TS>
